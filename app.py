00240424a1c1a6c5397da80884c3297f8dce109c
meetk5 <65046405+meetk5@users.noreply.github.com>
2021-09-23 10:51:45 -0400

    Updated app.py to import socket

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import os
+import socket
 
 from flask import Flask, jsonify, render_template, redirect
 
